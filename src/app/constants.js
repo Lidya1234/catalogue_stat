0ce10f3ef26efdd1506729fe1c20f6c3c80ead92
Lidya1234 <lidya94gher@gmail.com>
2021-05-06 23:13:25 +0300

    Add constants to constants.js

---
+++ src/app/constants.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://pokeapi.co/api/v2/pokemon';
+export const HTTP_STATUS = Object.freeze({
+    PENDING: 'PENDING',
+    FULLFILLED: 'FULLFILLED',
+    REJECTED: 'REJECTED'
+})
