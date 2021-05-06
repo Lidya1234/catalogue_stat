b2a2764a3d6b76459d66e47928a9602b8adf895c
Lidya1234 <lidya94gher@gmail.com>
2021-05-06 23:38:38 +0300

    Add image to catalogue component

--- src/components/catalogue.js
+++ src/components/catalogue.js
@@ -4,15 +4,12 @@ import { DISPLAYLIST, FILTERLIST } from '../reducers/catalogueSlice'
 import Card from 'react-bootstrap/Card';
 
 
-const Catalogue = ( {key ,catalogue} ) =>
+const Catalogue = ( { key ,item, name } ) =>
 {
 return(
-    <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>{ key }</Card.Title>
-     </Card.Body>
-   </Card>
+  
+    <img src={name} />
+  
     );
 }
 export default Catalogue;
