f941969d94e6c4c3a022a4f8b53c6eb2c5d7549c
Lidya1234 <lidya94gher@gmail.com>
2021-05-06 23:19:22 +0300

    Add the rendering of catalogue componenet

--- src/containers/CatalogueList.js
+++ src/containers/CatalogueList.js
@@ -2,34 +2,31 @@ import React,{ useEffect }  from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { DISPLAYLIST, FILTERLIST } from '../reducers/catalogueSlice'
 import Catalogue from '../components/Catalogue'
-import fetchCatalogs from '../helpers/fetchCatalogs'
+import { fetchCatalogs } from '../reducers/catalogueSlice'
 import CatalogueFilter from '../components/CatalogueFilter'
-const CatalogueList = () => {
-    const {catalogues, cataloguesLoading, catalogueLoading, catalogue, filter } = useSelector((state) => state.catalog)
+import CardGroup from 'react-bootstrap/CardGroup';
 
-    // const cataloguesLoading =  useSelector((state) => state.cataloguesLoading.value)
-console.log (catalogues)
-    // const catalogues = useSelector((state) => state.catalogues.value)
-    // const  catalogueLoading = useSelector((state) => state. catalogueLoading.value)
+const CatalogueList = () => {
+  
+// const catalogues = useSelector((state) =>state.catalog)
     const dispatch = useDispatch()
 useEffect(() =>
 {
-  if (catalogues === null || catalogues.length === 0)
-  {
-   fetchCatalogs()
-  }
-  else{
-    dispatch(DISPLAYLIST(catalogues))
-  }
-}, [fetchCatalogs, catalogues])
+   dispatch(fetchCatalogs())
   
-  const filtered = filter === 'ALL' ? catalogues : catalogues.filter(item => item.name.includes(filter))
-  const addCatalog =( catalog ) => {<Catalogue key ={catalog.id} item={catalog} />}
+},[])
+const {catalogues, cataloguesLoading, catalogueLoading, catalogue, filter } = useSelector((state) => state.catalog)
+console.log (catalogues)
+  // const filtered = filter === 'ALL' ? catalogues : catalogues.filter(item => catalogue[item].strCategory.includes(filter))
+  //const addCatalog =( catalog ) => {<Catalogue key ={catalog.idCategory} item={catalog} />}
 
   return (
     <div>
      <CatalogueFilter /> 
-     {filtered.map(addCatalog) }
+     <div className= "grid sec-2-grid ">
+     {catalogues.map((catalog) => <Catalogue key ={catalog.idCategory} item={catalog.strCategory}  name = {catalog.strCategoryThumb}/>) }
+     </div>
+
     </div>
   )
 }
