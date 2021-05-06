65984c4d1ae325f1b20af6b4d19bc4020f36d84c
Lidya1234 <lidya94gher@gmail.com>
2021-05-06 23:16:28 +0300

    Add another async function

--- src/reducers/catalogueSlice.js
+++ src/reducers/catalogueSlice.js
@@ -1,33 +1,70 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { API_URL, HTTP_STATUS} from '../app/constants';
+import axios from 'axios'
 
+ export const fetchCatalogs = createAsyncThunk(
+   'catalogue/fetchCatalogs', async () =>{
+     
+    const { data } = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+    console.log(data)
+     return data.categories
+   }
+ )
+
+
+ export const fetchCatalog = createAsyncThunk(
+  'catalogue/fetchCatalog', async () =>{
+    
+   const { data } = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+   console.log(data)
+    return data.categories
+  }
+)
 export const catalogueSlice = createSlice({
   name: 'catalogue',
   initialState: {
     catalogues: [],
-    cataloguesLoading: true,
+    cataloguesLoading: null,
    
-    catalogueLoading: true,
+    catalogueLoading: null,
     catalogue: {},
     filter: 'All'
   },
   reducers: {
-    DISPLAYLIST: (state, action) => ({
-      cataloguesLoading: false,
+    // DISPLAYLIST: (state, action) => ({
+    //   cataloguesLoading: false,
   
-      catalogues: state.catalogues
-    }),
-    FILTERLIST: (state, action) => ({
-      cataloguesLoading: false,
+    //   catalogues: state.catalogues
+    // }),
+    // FILTERLIST: (state, action) => ({
+    //   cataloguesLoading: false,
      
-      catalogues: [...action.payload]
-    }),
-    FETCHDETAILS: (state ,action) => ({
-      cataloguesLoading: true,
-      catalogueLoading: false,
-      catalogue: action.payload,
-    }),
+    //   catalogues: [...action.payload]
+    // }),
+    // FETCHDETAILS: (state ,action) => ({
+    //   cataloguesLoading: true,
+    //   catalogueLoading: false,
+    //   catalogue: action.payload,
+    // }),
  
   },
+
+  extraReducers: {
+    [fetchCatalogs.pending](state){
+      console.log('try')
+      state.cataloguesLoading =HTTP_STATUS.PENDING
+      state.catalogues = state.catalogues
+      
+    },
+    [fetchCatalogs.fulfilled](state, action){
+      state.cataloguesLoading =HTTP_STATUS.FULLFILLED
+      state.catalogues = action.payload
+    },
+    [fetchCatalogs.rejected](state){
+      state.cataloguesLoading =HTTP_STATUS.REJECTED
+
+    },
+    },
 })
 
 // Action creators are generated for each case reducer function
