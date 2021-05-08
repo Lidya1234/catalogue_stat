ff78661bab9d4cc0c8f21af7dcfaedb84a5be9b3
Lidya1234 <lidya94gher@gmail.com>
2021-05-08 22:52:40 +0300

    Add footer component

---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+import '../style.css';
+
+const Footer = () => (
+  <div className="footer">
+    <p className="footer-txt"> All Rights Reserved Lidya.G @2021</p>
+  </div>
+);
+export default Footer;
