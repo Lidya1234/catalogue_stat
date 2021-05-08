2c504e2f4fa5fd4a0f9024f7cd780c60210155f9
Lidya1234 <lidya94gher@gmail.com>
2021-05-08 23:10:08 +0300

    remove unecessary assignments and commented code

--- src/reducers/catalogueSlice.js
+++ src/reducers/catalogueSlice.js
@@ -1,44 +1,41 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { API_URL, HTTP_STATUS} from '../app/constants';
-import axios from 'axios'
+import axios from 'axios';
+import { API_URL, HTTP_STATUS } from '../app/constants';
 
- export const fetchCatalogs = createAsyncThunk(
-   'catalogue/fetchCatalogs', async () =>{
-     
-    const { data } = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-    console.log(data)
-     return data.categories
-   }
- )
+export const fetchCatalogs = createAsyncThunk(
+  'catalogue/fetchCatalogs', async () => {
+    const { data } = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+    console.log(data);
+    return data.categories;
+  },
+);
 
-
- export const fetchCatalog = createAsyncThunk(
-  'catalogue/fetchCatalog', async (name) =>{
-    
-   const { data } = await axios.get(`www.themealdb.com/api/json/v1/1/filter.php?c=${name}`)
-   console.log(data)
-    return data
-  }
-)
+export const fetchCatalog = createAsyncThunk(
+  'catalogue/fetchCatalog', async () => {
+    const { data } = await axios.get('www.themealdb.com/api/json/v1/1/list.php?c=list');
+    console.log(data);
+    return data;
+  },
+);
 export const catalogueSlice = createSlice({
   name: 'catalogue',
   initialState: {
     catalogues: [],
     cataloguesLoading: null,
-   
+
     catalogueLoading: null,
     catalogue: {},
-    filter: 'ALL'
+    filter: 'ALL',
   },
   reducers: {
     CHANGE_FILTER: (state, action) => ({
       filter: action.payload,
-      catalogues: state.catalogues
+      catalogues: state.catalogues,
 
     }),
     // FILTERLIST: (state, action) => ({
     //   cataloguesLoading: false,
-     
+
     //   catalogues: [...action.payload]
     // }),
     // FETCHDETAILS: (state ,action) => ({
@@ -46,42 +43,36 @@ export const catalogueSlice = createSlice({
     //   catalogueLoading: false,
     //   catalogue: action.payload,
     // }),
- 
+
   },
 
   extraReducers: {
-    [fetchCatalogs.pending](state){
-      console.log('try')
-      state.cataloguesLoading =HTTP_STATUS.PENDING
-      
-      
+    [fetchCatalogs.pending](state) {
+      console.log('try');
+      state.cataloguesLoading = HTTP_STATUS.PENDING;
     },
-    [fetchCatalogs.fulfilled](state, action){
-      state.cataloguesLoading =HTTP_STATUS.FULLFILLED
-      state.catalogues = action.payload
+    [fetchCatalogs.fulfilled](state, action) {
+      state.cataloguesLoading = HTTP_STATUS.FULLFILLED;
+      state.catalogues = action.payload;
     },
-    [fetchCatalogs.rejected](state){
-      state.cataloguesLoading =HTTP_STATUS.REJECTED
-
+    [fetchCatalogs.rejected](state) {
+      state.cataloguesLoading = HTTP_STATUS.REJECTED;
     },
-    [fetchCatalog.pending](state){
-      console.log('try')
-      state.catalogueLoading =HTTP_STATUS.PENDING
-     
-      
+    [fetchCatalog.pending](state) {
+      console.log('try');
+      state.catalogueLoading = HTTP_STATUS.PENDING;
     },
-    [fetchCatalog.fulfilled](state, action){
-      state.catalogueLoading =HTTP_STATUS.FULLFILLED
-      state.catalogue = action.payload
+    [fetchCatalog.fulfilled](state, action) {
+      state.catalogueLoading = HTTP_STATUS.FULLFILLED;
+      state.catalogue = action.payload;
     },
-    [fetchCatalog.rejected](state){
-      state.catalogueLoading =HTTP_STATUS.REJECTED
-
+    [fetchCatalog.rejected](state) {
+      state.catalogueLoading = HTTP_STATUS.REJECTED;
     },
-    },
-})
+  },
+});
 
 // Action creators are generated for each case reducer function
-export const {  CHANGE_FILTER, FILTERLIST} = catalogueSlice.actions
+export const { CHANGE_FILTER, FILTERLIST } = catalogueSlice.actions;
 
 export default catalogueSlice.reducer;
