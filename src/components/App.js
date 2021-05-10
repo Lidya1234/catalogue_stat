bd1193f189be79528163ae3d45a6446b66645aad
Lidya1234 <lidya94gher@gmail.com>
2021-05-10 13:24:22 +0300

    Add alt attribute to images

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Nav from './Nav';
+
 import CatalogueList from '../containers/CatalogueList';
-import Catalogue from './Catalogue';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style.css';
 
--- src/components/SingleCatalogue.js
+++ src/components/SingleCatalogue.js
@@ -1,11 +1,20 @@
+import { PropTypes } from 'prop-types';
+
 const SingleCatalogue = ({
   key, item, name, description,
 }) => (
   <div className="single-catalog card">
     <h1 className="item-name">{item}</h1>
-    <img src={name} />
+    <img src={name} alt={key} />
 
     <h5><i>{description}</i></h5>
   </div>
 );
+
+SingleCatalogue.propTypes = {
+  key: PropTypes.string.isRequired,
+  item: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 export default SingleCatalogue;
