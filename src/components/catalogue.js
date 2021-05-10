3e4c85afd17ecc81de2c37a38e2ac545c5e65f20
Lidya1234 <lidya94gher@gmail.com>
2021-05-10 17:00:38 +0300

    Add margin bottom to item-list css class

--- src/components/catalogue.js
+++ src/components/catalogue.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
-
+import '../style.css';
 import { useDispatch } from 'react-redux';
 import { CHANGE_FILTER } from '../reducers/catalogueSlice';
 
 const Catalogue = ({ key, item, name }) => {
   const dispatch = useDispatch();
   return (
-    <div>
+    <Link to="/Singlelist" onClick={() => dispatch(CHANGE_FILTER(item))}>
+      <img src={name} alt={key} />
+      <h1 className="item-name">{item}</h1>
+    </Link>
 
-      <Link to="/Singlelist" onClick={() => dispatch(CHANGE_FILTER(item))}>
-        <img src={name} alt={key} />
-        <h1 className="item-name">{item}</h1>
-      </Link>
-
-    </div>
   );
 };
 
