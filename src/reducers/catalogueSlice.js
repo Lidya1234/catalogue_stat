9e6f53f3b5bef7836b6cf452f42f329c665b97e9
Lidya1234 <lidya94gher@gmail.com>
2021-05-10 13:16:09 +0300

    Change the API in the async function

--- src/reducers/catalogueSlice.js
+++ src/reducers/catalogueSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { API_URL, HTTP_STATUS } from '../app/constants';
+import { HTTP_STATUS } from '../app/constants';
 
 export const fetchCatalogs = createAsyncThunk(
   'catalogue/fetchCatalogs', async () => {
@@ -12,7 +12,7 @@ export const fetchCatalogs = createAsyncThunk(
 
 export const fetchCatalog = createAsyncThunk(
   'catalogue/fetchCatalog', async () => {
-    const { data } = await axios.get('www.themealdb.com/api/json/v1/1/list.php?c=list');
+    const { data } = await axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
     console.log(data);
     return data;
   },
@@ -33,19 +33,9 @@ export const catalogueSlice = createSlice({
       catalogues: state.catalogues,
 
     }),
-    // FILTERLIST: (state, action) => ({
-    //   cataloguesLoading: false,
-
-    //   catalogues: [...action.payload]
-    // }),
-    // FETCHDETAILS: (state ,action) => ({
-    //   cataloguesLoading: true,
-    //   catalogueLoading: false,
-    //   catalogue: action.payload,
-    // }),
 
   },
-
+  /* eslint-disable no-param-reassign */
   extraReducers: {
     [fetchCatalogs.pending](state) {
       console.log('try');
@@ -58,20 +48,20 @@ export const catalogueSlice = createSlice({
     [fetchCatalogs.rejected](state) {
       state.cataloguesLoading = HTTP_STATUS.REJECTED;
     },
-    [fetchCatalog.pending](state) {
-      console.log('try');
-      state.catalogueLoading = HTTP_STATUS.PENDING;
-    },
-    [fetchCatalog.fulfilled](state, action) {
-      state.catalogueLoading = HTTP_STATUS.FULLFILLED;
-      state.catalogue = action.payload;
-    },
-    [fetchCatalog.rejected](state) {
-      state.catalogueLoading = HTTP_STATUS.REJECTED;
-    },
+    // [fetchCatalog.pending](state) {
+    //   console.log('try');
+    //   state.catalogueLoading = HTTP_STATUS.PENDING;
+    // },
+    // [fetchCatalog.fulfilled](state, action) {
+    //   state.catalogueLoading = HTTP_STATUS.FULLFILLED;
+    //   state.catalogue = action.payload;
+    // },
+    // [fetchCatalog.rejected](state) {
+    //   state.catalogueLoading = HTTP_STATUS.REJECTED;
+    // },
   },
 });
-
+/* eslint-enable no-param-reassign */
 // Action creators are generated for each case reducer function
 export const { CHANGE_FILTER, FILTERLIST } = catalogueSlice.actions;
 
