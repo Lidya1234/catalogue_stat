0ba8af35a09e3b4eb887b597f8e22b903df27ff4
Lidya1234 <lidya94gher@gmail.com>
2021-05-10 13:20:36 +0300

    Add the loading component to singelist

--- src/containers/Singlelist.js
+++ src/containers/Singlelist.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import Loading from '../components/Loading';
 import SingleCatalogue from '../components/SingleCatalogue';
 import { fetchCatalogs } from '../reducers/catalogueSlice';
 
@@ -12,18 +12,29 @@ const Singlelist = () => {
     dispatch(fetchCatalogs());
   }, []);
   const {
-    catalogues, catalogue, filter,
+    catalogues, cataloguesLoading, catalogue, filter,
   } = useSelector((state) => state.catalog);
 
   console.log(catalogue);
 
   const filtered = filter === 'ALL' ? catalogues : catalogues.filter((catalog) => catalog.strCategory.includes(filter));
-
+  if (cataloguesLoading === 'PENDING') {
+    return (
+      <Loading />
+    );
+  }
   return (
     <div>
 
-      <div>
-        {filtered.map((catalog) => <SingleCatalogue key={catalog.idCategory} item={catalog.strCategory} name={catalog.strCategoryThumb} description={catalog.strCategoryDescription} />) }
+      <div className="item-list">
+        {filtered.map((catalog) => (
+          <SingleCatalogue
+            key={catalog.idCategory}
+            item={catalog.strCategory}
+            name={catalog.strCategoryThumb}
+            description={catalog.strCategoryDescription}
+          />
+        )) }
       </div>
     </div>
 
