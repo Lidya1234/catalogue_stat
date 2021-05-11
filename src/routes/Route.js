b98d4fafdf134c42c2bdab36cde45053e33e365b
Lidya1234 <lidya94gher@gmail.com>
2021-05-11 15:06:53 +0300

    Remove App.js and replace it with catalogueList

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from '../components/App';
+// import App from '../components/App';
 import Footer from '../components/footer';
+import CatalogueList from '../containers/CatalogueList';
 import Singlelist from '../containers/Singlelist';
 import Nav from '../components/Nav';
 import '../style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Routes = () => (
   <BrowserRouter>
     <Nav />
     <Switch>
-      <Route exact path="/" component={App} />
+      <Route exact path="/" component={CatalogueList} />
       <Route exact path="/Singlelist" component={Singlelist} />
 
     </Switch>
