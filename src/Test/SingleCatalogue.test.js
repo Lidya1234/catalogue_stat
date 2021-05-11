69776b0f69ab923ae865674266e945f9ac1bf851
Lidya1234 <lidya94gher@gmail.com>
2021-05-11 16:16:31 +0300

    Remove empty line from SingleCatalogue.test.js

--- src/Test/SingleCatalogue.test.js
+++ src/Test/SingleCatalogue.test.js
@@ -7,7 +7,7 @@ import SingleCatalogue from '../components/SingleCatalogue';
 
 describe('The singleCatalogue component should render with this information', () => {
   const category = {
- 
+
     strCategory: 'Beef',
     strCategoryDescription: 'Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times.[1] Beef is a source of high-quality protein and essential nutrients.[2]',
     strCategoryThumb: 'https://www.themealdb.com/images/category/beef.png',
@@ -15,7 +15,6 @@ describe('The singleCatalogue component should render with this information', ()
   render(
     <Router>
       <SingleCatalogue
-
         item={category.strCategory}
         name={category.strCategoryThumb}
         description={category.strCategoryDescription}
